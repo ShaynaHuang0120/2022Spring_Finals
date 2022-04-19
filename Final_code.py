63c7c3623e26b455722de6477ecf665be25c3dcc
ShaynaHuang0120 <shayna308110@gmail.com>
2022-04-19 00:46:32 -0500

    updated1

--- Final_code.py
+++ Final_code.py
@@ -1,2 +1,53 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+def read_data(file: str) -> pd.DataFrame:
+    """
+    this function is used to turn csv file into dataframe
+    :param file: input csv file name
+    :return: dataframe of the input csv file
+    """
+    df = pd.read_csv(file)
+    return df
+
+def data_process(df:pd.DataFrame, column,value) -> pd.DataFrame:
+    """
+    this function used to import and process the data
+    :param df: dataframe that need to process
+    :param column: column that need to clean and convert data type
+    :param value: rows with the value will be deleted
+    :return: dataframe that are processed
+    """
+    df1= df.dropna()
+    df2 = df1[df1[column].notna()]
+    df2.drop(df2[df2[column] == value].index, inplace = True)
+    df2[column] = df2[column].astype(int)
+    return df2
+
+
+def total_pitstop(pitstop_table: pd.DataFrame):
+    """
+    this function used to calculate the number of pitstop for each driver per race
+    :param pitstop_table: dataframe that need to find the total pit stop rows
+    :return: dataframe shows the total number of pit stop of each driver and race
+    >>> df = pd.DataFrame({"raceId":[1,1,1],"driverId":[1,1,1], "stop":[1,2,3]})
+    >>> total_pitstop(df)
+       raceId  driverId  stop
+    0       1         1     3
+    """
+    pitstop_df = pitstop_table[["raceId", "driverId", "stop"]]
+    pitstop_groupby = pitstop_df.groupby(["raceId", "driverId"], as_index=False)["stop"].count()
+    # print(pitstop_groupby["stop"].sum())
+    pitstop_groupby.sort_values(by=["raceId", 'driverId'], inplace=True)
+    return (pitstop_groupby)
+
+
+
+
+if __name__ == '__main__':
+    pitstops_file = read_data("data/pit_stops.csv")
+    result = total_pitstop(pitstops_file)
+    process = results_data_process(results_file, "position", "\\N")
+    print(process)
+
 
