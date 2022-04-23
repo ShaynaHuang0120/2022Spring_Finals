cb488eb4420ca360e7d589fbb695b3848f1f8488
andrewmo <andrewmo1002@gmail.com>
2022-04-23 17:36:50 -0500

    updated: merge_data function

--- Final_code.py
+++ Final_code.py
@@ -1,6 +1,56 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
+import re
+from typing import List
+
+
+def merge_data(_df_list: List[pd.DataFrame]) -> pd.DataFrame:
+    """
+    merges the dataframes according to their primary/foreign keys
+    :param _df_list:
+    :return:
+    """
+    # set up internal parameters
+    suffixes = ['_1', '_2']
+    r = re.compile('\w*Id')
+
+    # set up variables based on input
+    num = len(_df_list)  # length of the list
+    to_select = list(range(1, num))  # list of indexes of dataframe to be merged
+    remaining = set(range(1, num))  # set of remaining indexes of dataframes yet to be merged
+
+    # get all the '<something>id' columns for each dataframe
+    id_list = [set(filter(r.match, table)) for table in _df_list]
+    # set up the merged dataframe, 'mg_df'
+    mg_df = _df_list[0]
+    mg_id = id_list[0]  # set the id set of the merged dataframe as the first dataframe's id set
+
+    # start merging dataframes
+    while True:
+        merge_flag = 0  # flag indicating if a while-true run has merged any new dataframe
+        for index in to_select:
+            intersect = mg_id.intersection(id_list[index])
+            all_col_intersect = set(mg_df.columns).intersection(set(_df_list[index].columns))
+            # iterate through the remaining dataframe
+            # and merge those with common id(s)
+            if (index in remaining) and intersect:
+                if all_col_intersect == intersect:
+                    mg_df = pd.merge(mg_df, _df_list[index], on=list(intersect), how='left')
+                else:
+                    # if there are other common columns than the ids, set suffixes
+                    mg_df = pd.merge(mg_df, _df_list[index], on=list(intersect), how='left', suffixes=suffixes)
+                # add the new ids into the total id set of the merged dataframe
+                mg_id = mg_id.union(set(id_list[index]))
+                remaining.remove(index)  # remove the index of the added dataframe
+                merge_flag = 1
+        if not remaining:
+            break  # break if there is no more dataframe to be added
+        if not merge_flag:  # if no new dataframe was merged in this while-true run
+            print('Error: no common "id" columns found')
+            break  # breaks
+    return mg_df
+
 
 def read_data(file: str) -> pd.DataFrame:
     """
@@ -11,7 +61,8 @@ def read_data(file: str) -> pd.DataFrame:
     df = pd.read_csv(file)
     return df
 
-def data_process(df:pd.DataFrame, column,value) -> pd.DataFrame:
+
+def data_process(df: pd.DataFrame, column, value) -> pd.DataFrame:
     """
     this function used to import and process the data
     :param df: dataframe that need to process
@@ -52,7 +103,7 @@ def pitstop_boxplot(df_a, df_b, merge_key: list, boxplot_data: list):
     :param boxplot_data: data used for boxplot
     :return: boxplot shows the race result of different amount of pit stop
     """
-    joined_table = df_a.merge(df_b, on = merge_key)
+    joined_table = df_a.merge(df_b, on=merge_key)
     boxplot_base = joined_table[boxplot_data]
     boxplot = boxplot_base.boxplot(by="stop")
     # return boxplot
@@ -78,10 +129,12 @@ def tables_combined(df_a, df_b, df_c, merge_key1: list, merge_key2, time_conditi
     # print(decade_table[['year', "circuitId"]])
     return decade_table
 
-#Question: do we need to consider circuit id?
-def stop_chart(combined_table:pd.DataFrame, pit_stop: int):
+
+# Question: do we need to consider circuit id?
+def stop_chart(combined_table: pd.DataFrame, pit_stop: int):
     """
 
+    :param combined_table:
     :param df_a: table to join
     :param df_b: table to join
     :param df_c: table to join
@@ -91,20 +144,17 @@ def stop_chart(combined_table:pd.DataFrame, pit_stop: int):
     :return: bar chart shows the count of positions in history records when taking 1, 2 or 3 pit stops
     """
 
-    for i in range(1, pit_stop+1):
-        position_count = combined_table[combined_table['stop'] == i].groupby(['position'])["driverId"].count().reset_index(name='count')
+    for i in range(1, pit_stop + 1):
+        position_count = combined_table[combined_table['stop'] == i].groupby(['position'])[
+            "driverId"].count().reset_index(name='count')
         position_count.plot.bar(x='position', y='count', fontsize='9')
         # plt.xticks(rotation='90')
         stop = "pit stop = " + str(i)
         plt.title(stop)
         plt.show()
 
+
 # def circuit_stop_chart(combined_table:pd.DataFrame, pit_stop: int):
-
-
-
-
-
 
 
 if __name__ == '__main__':
@@ -113,10 +163,7 @@ if __name__ == '__main__':
     races_file = read_data("dataset/races.csv")
     total_pitstop = total_pitstop(pitstops_file)
     process = data_process(results_file, "position", "\\N")
-    pitstop_boxplot(total_pitstop, process, ["raceId", "driverId"],["stop", "position"])
-    combined_table = tables_combined(process,total_pitstop, races_file, ["raceId", "driverId"], ['raceId'], 10)
+    pitstop_boxplot(total_pitstop, process, ["raceId", "driverId"], ["stop", "position"])
+    combined_table = tables_combined(process, total_pitstop, races_file, ["raceId", "driverId"], ['raceId'], 10)
     stop_chart(combined_table, 3)
     # circuit_stop_chart(combined_table, 3)
-
-
-
